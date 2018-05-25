382edd48d56b8c5263f5e3dee6ecb18ed09e371d
Czapel <czapelxd@gmail.com>
2018-05-25 02:11:04 +0200

    Initial commit

---
+++ assets/js/hideANDseek.js
@@ -0,0 +1,61 @@
+function aboutF() {
+    document.getElementById("education").style.display = 'none';
+    document.getElementById("exp").style.display = 'none';
+    document.getElementById("skills").style.display = 'none';
+    document.getElementById("projects").style.display = 'none';
+    document.getElementById("portfolio").style.display = 'none';
+
+    document.getElementById("about").style.display = 'block';
+}
+
+function eduF() {
+    document.getElementById("about").style.display = 'none';
+    document.getElementById("exp").style.display = 'none';
+    document.getElementById("skills").style.display = 'none';
+    document.getElementById("projects").style.display = 'none';
+    document.getElementById("portfolio").style.display = 'none';
+
+    document.getElementById("education").style.display = 'block';
+}
+
+function expF() {
+    document.getElementById("about").style.display = 'none';
+    document.getElementById("education").style.display = 'none';
+    document.getElementById("skills").style.display = 'none';
+    document.getElementById("projects").style.display = 'none';
+    document.getElementById("portfolio").style.display = 'none';
+
+    document.getElementById("exp").style.display = 'block';
+}
+
+function skillsF() {
+    document.getElementById("about").style.display = 'none';
+    document.getElementById("education").style.display = 'none';
+    document.getElementById("exp").style.display = 'none';
+    document.getElementById("projects").style.display = 'none';
+    document.getElementById("portfolio").style.display = 'none';
+
+    document.getElementById("skills").style.display = 'block';
+}
+
+function projectsF() {
+    document.getElementById("about").style.display = 'none';
+    document.getElementById("education").style.display = 'none';
+    document.getElementById("skills").style.display = 'none';
+    document.getElementById("exp").style.display = 'none';
+    document.getElementById("portfolio").style.display = 'none';
+
+    document.getElementById("projects").style.display = 'block';
+}
+
+function portfolioF() {
+    document.getElementById("about").style.display = 'none';
+    document.getElementById("education").style.display = 'none';
+    document.getElementById("skills").style.display = 'none';
+    document.getElementById("exp").style.display = 'none';
+    document.getElementById("projects").style.display = 'none';
+
+    document.getElementById("portfolio").style.display = 'block';
+}
+
+
